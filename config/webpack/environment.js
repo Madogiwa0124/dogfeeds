9320dc04327d916a5973a3287980c78ab38e7b78
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-02-17 22:06:52 +0900

    add eslint setting :cop:

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
-const vue =  require('./loaders/vue')
+const vue = require('./loaders/vue')
+const eslint =  require('./loaders/eslint')
 
 environment.loaders.append('vue', vue)
 environment.loaders.append('typescript', typescript)
+environment.loaders.append('eslint', eslint)
 module.exports = environment
