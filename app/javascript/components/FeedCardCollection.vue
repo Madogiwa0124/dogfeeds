b9ded3a1f8ef9a8808b255f5281005c597fb7c23
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-03-03 00:44:36 +0900

    add pageloader

--- app/javascript/components/FeedCardCollection.vue
+++ app/javascript/components/FeedCardCollection.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="entries is-multiline columns">
+    <page-loader :init_is_loading="isLoading" />
     <div
       v-for="feed in feeds"
       :key="feed.id"
@@ -14,23 +15,28 @@
 </template>
 <script>
 import FeedCard from './FeedCard';
+import PageLoader from './PageLoader';
 import axios from 'axios';
 
 const INTERVAL_TIME = 600000;
 
 export default {
   name: 'FeedCardCollection',
-  components: { FeedCard },
+  components: { FeedCard, PageLoader },
   props: ['init_feeds', 'init_last_entries'],
   data: function () {
     return {
       feeds: this.init_feeds,
       last_entries: this.init_last_entries,
       intervalId: 0,
+      isLoading: true
     };
   },
   mounted: function () {
     this.intervalId = setInterval(this.getFeeds, INTERVAL_TIME);
+    this.$nextTick(function () {
+      this.isLoading = false;
+    });
   },
   beforeDestroy: function () {
     clearInterval(this.intervalId);
---
+++ app/javascript/components/PageLoader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="pageloader"
+    :class="{ 'is-active': isLoading }"
+  >
+    <span class="title">Loading</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PageLoader',
+  components: { },
+  props: ['init_is_loading'],
+  data: function() {
+    return {
+      isLoading: this.init_is_loading
+    };
+  },
+  watch: {
+    init_is_loading: function() {
+      this.isLoading = this.init_is_loading;
+    }
+  },
+  methods: {}
+};
+</script>
+<style lang="scss">
+</style>
