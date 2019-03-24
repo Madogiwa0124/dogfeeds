0fab45debb8f9022d45eac5d0829ba975828a3be
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-03-24 10:21:26 +0900

    add model spec

--- spec/factories/boards.rb
+++ spec/factories/boards.rb
@@ -12,4 +12,10 @@ FactoryBot.define do
   factory :board do
     title { 'Board Title' }
   end
+
+  trait :with_feeds do
+    after(:create) do |board|
+      create_list(:board_feed, 3, board: board)
+    end
+  end
 end
