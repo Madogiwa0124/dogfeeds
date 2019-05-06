c15c9ea0856daa8dc5c20f259a07a548b6b8b744
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-05-06 14:49:32 +0900

    add tag input form

---
+++ app/javascript/components/TagsInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tags-input">
+    <label class="label">タグ</label>
+    <input
+      v-for="(tag, index) in tags"
+      :key="index"
+      type="hidden"
+      name="tags[]"
+      :value="tag.name"
+    >
+    <div class="tag-list">
+      <span
+        v-for="(tag, index) in tags"
+        :key="index"
+        class="tag is-primary"
+      >
+        {{ tag.name }}
+        <span
+          class="delete is-small"
+          @click="deleteTag(index)"
+        />
+      </span>
+    </div>
+    <input
+      v-model="tagName"
+      class="tag-input-area input is-small"
+      @keydown.enter="buildTag"
+    >
+    <p class="help">
+      Rssフィードのタグを入力してください。(任意)
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TagsInput',
+  components: {},
+  props: ['init_tags'],
+  data: function() {
+    return {
+      tagName: '',
+      tags: this.init_tags ? this.init_tags : []
+    };
+  },
+  methods: {
+    buildTag: function(event) {
+      event.preventDefault();
+      this.tags.push({name: this.tagName});
+      this.tagName = '';
+    },
+    deleteTag: function(index) {
+      this.tags.splice(index,1);
+    }
+  }
+};
+</script>
+<style lang="scss">
+</style>
