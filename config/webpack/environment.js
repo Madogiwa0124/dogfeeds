caccf5861e94059a9a586980b973789e3bfbe2f4
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-10-13 17:16:09 +0900

    bundle exec rails webpacker:install

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-const vue = require('./loaders/vue')
-const eslint =  require('./loaders/eslint')
 
-environment.loaders.append('vue', vue)
-environment.loaders.append('typescript', typescript)
-environment.loaders.append('eslint', eslint)
 module.exports = environment
