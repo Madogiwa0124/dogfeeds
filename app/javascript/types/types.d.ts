63b3d0c240e2946757d5bf9599dab320ac03c4a5
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-07-03 16:15:07 +0900

    :sparkles: TypeScriptで一部のコンポーネントを書き直してみた。

---
+++ app/javascript/types/types.d.ts
@@ -0,0 +1,31 @@
+export interface Feed {
+  id: number;
+  endpoint: string;
+  last_published_at: Date;
+  title: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface Entry {
+  id: number;
+  description: Text;
+  eye_catching_image: string;
+  link: string;
+  published_at: Date;
+  title: string;
+  created_at: Date;
+  updated_at: Date;
+  feed_id: number;
+}
+
+export interface FeedTag {
+  feed_id: number;
+  body: Text;
+}
+
+export interface FeedsResponse {
+  feeds: Feed[];
+  last_entries: Entry[];
+  tags: FeedTag[];
+}
