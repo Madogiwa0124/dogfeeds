9b46898b12bd3ff312daa0961e60c04fa5f83e70
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-07-20 22:13:14 +0900

    system spec用の設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,10 +13,26 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options
+  )
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.before(:each, type: :system) do |example|
+    example.metadata[:js] ? driven_by(:headless_chrome) : driven_by(:rack_test)
+  end
 end
