7b17bd4b753be89828f34bd40a36c422d5553d6b
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-07-22 18:20:31 +0900

    jsonシリアライズ用のClassを用意してCamelCase及びFeedにEntryとTagを含んだレスポンスを返すようにした。

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -2,18 +2,10 @@ class Api::FeedsController < ApplicationController
   PER_PAGE = 6
 
   def index
-    @feeds = Feed.search(params.dig(:query, :keyword)).recent
+    @feeds = Feed.preload(:last_entry, :tags)
+                 .search(params.dig(:query, :keyword)).recent
                  .pager(page: params[:page], per: PER_PAGE)
-    @last_entries = @feeds.preload(:last_entry).map(&:last_entry)
-    render json: { feeds: @feeds, last_entries: @last_entries, tags: tags }
-  end
-
-  private
-
-  def tags
-    taggings = FeedTagging.where(feed: @feeds).preload(:feed_tag)
-    taggings.map do |tagging|
-      { feed_id: tagging.feed_id, body: tagging.feed_tag.body }
-    end
+    response = @feeds.map { |feed| ::Api::Feed.new(feed: feed).attributes }
+    render json: response
   end
 end
---
+++ spec/models/api/feed_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Api::Feed, type: :model do
+  describe '#attribute' do
+    let!(:feed) { create(:feed) }
+
+    context 'Tag、Entryを保つ場合' do
+      let!(:entry) { create(:entry, feed: feed) }
+      let!(:tag) { create(:feed_tag) }
+
+      before { create(:feed_tagging, feed: feed, feed_tag: tag) }
+
+      it 'Tag、Entryを含んだFeedのHashが取得できること' do
+        expect(Api::Feed.new(feed: feed).attributes).to eq(
+          {
+            id: feed.id,
+            endpoint: feed.endpoint,
+            lastPublishedAt: feed.last_published_at.iso8601,
+            title: feed.title,
+            lastEntry: {
+              id: entry.id,
+              description: entry.description,
+              eyeCatchingImage: entry.eye_catching_image,
+              link: entry.link,
+              publishedAt: entry.published_at.iso8601,
+              title: entry.title,
+              feedId: entry.feed_id
+            },
+            tags: [{
+              id: tag.id,
+              body: tag.body,
+              feedId: feed.id
+            }]
+          }
+        )
+      end
+    end
+
+    context 'Tag、Entryが無い場合' do
+      it 'Tagは空配列、EntryはnilでFeedのHashが取得できること' do
+        expect(Api::Feed.new(feed: feed).attributes).to eq(
+          {
+            id: feed.id,
+            endpoint: feed.endpoint,
+            lastPublishedAt: feed.last_published_at.iso8601,
+            title: feed.title,
+            lastEntry: nil,
+            tags: []
+          }
+        )
+      end
+    end
+  end
+end
