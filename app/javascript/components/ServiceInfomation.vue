889956defa7bc83521d07e34978418836eb1901d
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-08-05 22:56:27 +0900

    サービス紹介は何度も読まないので閉じれるようにした。

---
+++ app/javascript/components/ServiceInfomation.vue
@@ -0,0 +1,44 @@
+<template>
+  <article class="service-infomation message">
+    <div class="message-header has-background-info">
+      <p>Dogfeedsとは？</p>
+      <button class="delete" aria-label="delete" @click="handleOnDelete" />
+    </div>
+    <div class="message-body">
+      <strong>
+        RSSフィードをまとめたRssフィードを作ることができます。 作り方は簡単なので、ぜひ作ってみてください🐶
+      </strong>
+      <br />
+      1. フィードを選択する<br />
+      2. まとめたフィード(ボード)に名前をつける<br />
+      3. ボードを作って、共有するなり、Slackチャンネルに追加するなりする！<br />
+    </div>
+  </article>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "SetviceInfomation",
+  methods: {
+    handleOnDelete: function () {
+      this.$emit("delete");
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+.message {
+  margin-bottom: 0em;
+
+  .delete {
+    &:hover {
+      background-color: hsl(348, 100%, 61%);
+    }
+  }
+
+  .message-body {
+    color: #000000;
+    background-color: #ffffff;
+  }
+}
+</style>
