8c896e8d14ff4ffe993c6a0cabf28d288af4acf0
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-12-29 13:59:44 +0900

    remove console.log

--- app/javascript/services/TagService.ts
+++ app/javascript/services/TagService.ts
@@ -5,7 +5,6 @@ const TAGS_API_ENDPOINT = "/api/tags";
 
 export async function getTags(query = "", params: object): Promise<Tag[]> {
   const response = await Client.get(TAGS_API_ENDPOINT + query, { params: params });
-  console.log(response.data);
   const tags: Tag[] = response.data;
   return tags;
 }
