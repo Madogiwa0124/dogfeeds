99a9ab9f69c5846aceaf2020df9f7038a32e9e4e
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-12-29 13:04:36 +0900

    sourcemapをrollbarへuploadするプラグインを有効化

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,14 +1,13 @@
 process.env.NODE_ENV = "production";
 
-// const RollbarSourceMapPlugin = require("./helpers/rollbarSourceMapConfig.js");
 const { merge } = require("webpack-merge");
+const RollbarSourceMapPlugin = require("./helpers/rollbarSourceMapConfig.js");
 const common = require("../../webpack.common.js");
-// const isUploadSourceMap = process.env.UPLOAD_SOURCEMAP === "true";
-
-// const plugins = isUploadSourceMap ? common.plugins.concat(RollbarSourceMapPlugin) : common.plugins;
-
-// console.log(common.plugins);
+const isUploadSourceMap = process.env.UPLOAD_SOURCEMAP === "true";
+const plugins = isUploadSourceMap ? [RollbarSourceMapPlugin] : [];
 
 module.exports = merge(common, {
   mode: process.env.NODE_ENV,
+  devtool: "hidden-source-map",
+  plugins: plugins,
 });
