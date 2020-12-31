5217c48ff94881320671dcf22c8796aba089535e
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-12-31 10:37:07 +0900

    APIでidを使ってFeedを検索出来るようにした

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -2,9 +2,8 @@ class Api::FeedsController < ApplicationController
   PER_PAGE = 12
 
   def index
-    @feeds = Feed.preload(:last_entry, :tags)
-                 .search(params.dig(:query, :keyword)).recent
-                 .pager(page: params[:page], per: PER_PAGE)
+    @feeds = Feed.preload(:last_entry, :tags).search(search_params).recent
+    @feeds = @feeds.pager(page: params[:page], per: PER_PAGE) if params[:page].present?
     response = @feeds.map { |feed| ::Api::Feed.new(feed: feed).attributes }
     render json: response
   end
@@ -12,5 +11,11 @@ class Api::FeedsController < ApplicationController
   def show
     feed = ::Api::Feed.new(feed: Feed.preload(:tags, :last_entry).find(params[:id]))
     render json: feed.attributes
+  end
+
+  private
+
+  def search_params
+    { keyword: params.dig(:query, :keyword), ids: params.dig(:query, :ids) }
   end
 end
