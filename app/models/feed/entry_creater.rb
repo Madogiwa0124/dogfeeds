6d752d4f7190db2b2eed145e54865e93d31ed05a
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-03-13 14:01:21 +0900

    create_withを使ってinsert_allを使ったbulk insallのtimestamp設定処理をリファクタリング

--- app/models/feed/entry_creater.rb
+++ app/models/feed/entry_creater.rb
@@ -5,12 +5,12 @@ class Feed::EntryCreater
     @feed = feed
   end
 
-  def execute!
+  def execute!(now: Time.current)
     ActiveRecord::Base.transaction do
       Entry.where(feed: feed).delete_all
       # NOTE: validate済みのものを渡しているので警告を無効化
       # rubocop:disable Rails/SkipsModelValidations
-      Entry.insert_all!(target_entries)
+      Entry.create_with(created_at: now, updated_at: now).insert_all!(target_entries)
       # rubocop:enable Rails/SkipsModelValidations
       reloaded_feed = Feed.preload(:last_entry).find(feed.id)
       reloaded_feed.update!(last_published_at: reloaded_feed.last_entry.published_at)
@@ -19,9 +19,9 @@ class Feed::EntryCreater
 
   private
 
-  def target_entries(now: Time.current)
+  def target_entries
     feed.parsed_items.map do |item|
-      attributes = item.attributes.merge(feed_id: feed.id, created_at: now, updated_at: now)
+      attributes = item.attributes.merge(feed_id: feed.id)
       # valid?時のDBアクセスを抑制するためfeedのオブジェクトを代入する
       entry = Entry.new(attributes).tap { |obj| obj.feed = feed }
       # insert_all!はcallbackを無視してvalidationが走らないのでvalidなものだけ返却する
