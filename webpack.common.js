9c2fea38d840522398f4f402cbd4327d7dde9059
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-05-29 17:14:36 +0900

    jestでも使うので.babelrcにbabelの設定を移した

--- webpack.common.js
+++ webpack.common.js
@@ -27,14 +27,6 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: [
-              // NOTE: `ReferenceError regeneratorRuntime is not defined`が発生していたので解消
-              // https://github.com/babel/babel/issues/9849#issuecomment-612595221
-              "@babel/plugin-transform-runtime",
-            ],
-          },
         },
       },
       {
