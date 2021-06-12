2051cf40a07b356e03f739d5f01750f0edc5588a
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-06-12 17:25:37 +0900

    GAでのtrackingを有効化

---
+++ app/javascript/packs/application/head.js
@@ -0,0 +1,12 @@
+// Global site tag (gtag.js) - Google Analytics
+// GAから転載したスクリプトのため無効化
+
+/* eslint-disable */
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", process.env.GOOLE_ANALYTICS_ID);
+/* eslint-enable */
