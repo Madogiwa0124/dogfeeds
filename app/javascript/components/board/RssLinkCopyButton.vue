06f87e75cd6fb6711989a3c3beeaad9551408ca6
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-06-12 17:27:09 +0900

    ボード作成・RSSリンクコピーをgtagでトラッキングしてみる

--- app/javascript/components/board/RssLinkCopyButton.vue
+++ app/javascript/components/board/RssLinkCopyButton.vue
@@ -11,6 +11,7 @@ import { Board } from "@js/types/types";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faRss } from "@fortawesome/free-solid-svg-icons";
+import { trackEvent } from "@js/services/Gtag";
 library.add(faRss);
 
 export default Vue.extend({
@@ -30,6 +31,11 @@ export default Vue.extend({
   methods: {
     handleOnRssLinkCopyClick: async function () {
       await navigator.clipboard.writeText(this.rssUrl);
+      trackEvent({
+        action: "copy_board_rss_link",
+        category: this.$options.name,
+        label: this.board.title,
+      });
       alert(`${this.rssUrl}をクリップボードにコピーしました！`);
     },
   },
